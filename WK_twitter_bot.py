9d1492766978df08b27e7143354a6c91ad675489
Utkarsh Sinha <utkarsh.sinha97@gmail.com>
2018-12-18 07:30:56 +0530

    First day second commit

--- WK_twitter_bot.py
+++ WK_twitter_bot.py
@@ -7,13 +7,39 @@ Created on Tue Dec 18 06:28:59 2018
 
 import tweepy
 
-CONSUMER_KEY='ysvqCFn7aEEpR6W8fNePMRxoK '
-CONSUMER_SECRET='NOg27fr7BHw2ZCbbsnyCTPp0zmjk16rk9C0w1MJGAfqVvMURXq '
+CONSUMER_KEY='ysvqCFn7aEEpR6W8fNePMRxoK'
+CONSUMER_SECRET='NOg27fr7BHw2ZCbbsnyCTPp0zmjk16rk9C0w1MJGAfqVvMURXq'
 ACCESS_KEY='223796937-DLIS14oo7dYXWUF2ondgAEYk1NL4F5GrcsiFSa3A'
-ACCESS_SECRET='HxElCbiak72rz8xwxLhD52xuCURXbzfbJuJIMyKjNVoJ7 '
+ACCESS_SECRET='HxElCbiak72rz8xwxLhD52xuCURXbzfbJuJIMyKjNVoJ7'
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api=tweepy.API(auth)
 
+#saving ids in a file to resolve redundant replying
+FILE_NAME='Last_seen_ids.txt'
+
+def get_last_seen_id(file_name):
+    f_read = open(file_name,'r')
+    last_seen_id=int(f_read.read().strip())
+    f_read.close();
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write=open(file_name,'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+
+last_seen_id=get_last_seen_id(FILE_NAME)
+mentions=api.mentions_timeline(last_seen_id, tweet_mode='extended')
+
+for mention in reversed(mentions):
+    print(str(mention.id)+' --> '+ mention.full_text)
+    last_seen_id = mention.id
+    store_last_seen_id(last_seen_id,FILE_NAME)
+    if '#helloworld' in mention.full_text.lower():
+        api.update_status('@' + mention.user.screen_name + ' Build you own castle! ', mention.id)
+    
 
